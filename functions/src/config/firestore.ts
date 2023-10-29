5caf1306fad22b56ca4e6f19cdc8c2b2bf02f8bb
Carlos Reis <carlos.henriquer287@gmail.com>
2023-10-29 13:18:44 -0300

    firestore

--- functions/src/config/firestore.ts
+++ functions/src/config/firestore.ts
@@ -6,17 +6,17 @@ dotenv.config({ path: "./.env" });
 
 const serviceAccount = {
   type: "service_account",
-  project_id: process.env.FIREBASE_PROJECT_ID,
-  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  project_id: process.env.FRB_PROJECT_ID,
+  private_key_id: process.env.FRB_PRIVATE_KEY_ID,
   private_key:
-    process.env.FIREBASE_PRIVATE_KEY?.split(String.raw`\n`).join("\n") || "",
-  client_email: process.env.FIREBASE_CLIENT_EMAIL,
-  client_id: process.env.FIREBASE_CLIENT_ID,
-  auth_uri: process.env.FIREBASE_AUTH_URI,
-  token_uri: process.env.FIREBASE_TOKEN_URI,
-  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
-  universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
+    process.env.FRB_PRIVATE_KEY?.split(String.raw`\n`).join("\n") || "",
+  client_email: process.env.FRB_CLIENT_EMAIL,
+  client_id: process.env.FRB_CLIENT_ID,
+  auth_uri: process.env.FRB_AUTH_URI,
+  token_uri: process.env.FRB_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FRB_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FRB_CLIENT_X509_CERT_URL,
+  universe_domain: process.env.FRB_UNIVERSE_DOMAIN,
 };
 
 initializeApp({
